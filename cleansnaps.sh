6c8c1c0879b33e21b199736e53c8de2816e6626f
King-Kingah <kingajames2015@gmail.com>
2023-05-20 10:07:48 +0300

    Script now can send a notification once it runs.

--- cleansnaps.sh
+++ cleansnaps.sh
@@ -7,3 +7,5 @@ LANG=C snap list --all | awk '/disabled/{print $1, $3}' |
     while read snapname revision; do
         snap remove "$snapname" --revision="$revision"
     done
+
+notify-send "Snaps purged!!!" 
